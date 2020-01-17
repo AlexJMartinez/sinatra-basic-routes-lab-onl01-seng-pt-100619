ff553292ce25e86794181b30e0701ec561da66db
Alex Martinez <alexandr3martin3z@gmail.com>
2020-01-17 05:35:11 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,15 +2,15 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get /name do
+  get '/name' do
     "My name is Alex"
   end
 
-  get /hometown do
+  get '/hometown' do
     "My hometown is Oceanside, CA"
   end
 
-  get /favorite-song do
+  get '/favorite-song' do
     "My favorite song is Such A Shame"
   end
 end
